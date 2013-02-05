ecc8072271cd5096215bcec720a031457c9d8d92
black_13 <jjosburn@gmail.com>
2013-02-04 21:29:40 -0600

    check in before beautification

---
+++ libsiod/foo.c
@@ -0,0 +1,3 @@
+#include "siod.h"
+
+LISP foo(LISP l);
---
+++ unit_test_consiod1/main.cpp
@@ -0,0 +1,9 @@
+/*
+This will unit test the basics of siod library.
+*/
+#include "gtest/gtest.h"
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
